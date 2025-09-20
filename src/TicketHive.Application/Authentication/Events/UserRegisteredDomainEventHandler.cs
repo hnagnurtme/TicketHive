d3705df312f3fde143801839adafd6bd58da5695
Trung Ánh <anhnon0106@gmail.com>
2025-09-20 19:27:33 +0700

    refactor(auth): config secret , add path for send email verify

--- src/TicketHive.Application/Authentication/Events/UserRegisteredDomainEventHandler.cs
+++ src/TicketHive.Application/Authentication/Events/UserRegisteredDomainEventHandler.cs
@@ -3,29 +3,33 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using TicketHive.Application.Common.Interfaces;
 using TicketHive.Domain.Events;
+using Microsoft.Extensions.Configuration;
 
-
-public class UserRegisteredDomainEventHandler 
+public class UserRegisteredDomainEventHandler
     : INotificationHandler<UserRegisteredDomainEvent>
 {
     private readonly ILogger<UserRegisteredDomainEventHandler> _logger;
 
     private readonly IEmailSender _emailSender;
 
+    private readonly IConfiguration _configuration;
+
     private readonly IJwtService _jwtService;
 
     public UserRegisteredDomainEventHandler(
         ILogger<UserRegisteredDomainEventHandler> logger,
+        IConfiguration configuration,
         IJwtService jwtService,
         IEmailSender emailSender)
     {
         _logger = logger;
         _jwtService = jwtService;
+        _configuration = configuration;
         _emailSender = emailSender;
     }
     public Task Handle(UserRegisteredDomainEvent notification, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("User registered with ID: {UserId} and Email: {Email}", 
+        _logger.LogInformation("User registered with ID: {UserId} and Email: {Email}",
             notification.UserId, notification.Email);
 
         // Generate email verification token
@@ -34,11 +38,14 @@ public class UserRegisteredDomainEventHandler
             new Claim(ClaimTypes.Email, notification.Email)
         };
         var token = _jwtService.GenerateToken(claims, TimeSpan.FromMinutes(15));
-        // Gửi email verification (giả sử bạn đã cấu hình IEmailSender)
+        // xác định path cho link verification láy từ env
 
-        var verificationLink = $"https://localhost:5000/api/auth/verify-email?Email={notification.Email}&Token={token}";
+        var baseUrl = _configuration["App:BaseUrl"]
+              ?? throw new InvalidOperationException("BaseUrl is not configured.");
+
+        var verificationLink = $"{baseUrl}/api/auth/verify-email?Email={notification.Email}&Token={token}";
         var subject = "Welcome to TicketHive!";
-        var message = $"Thank you for registering, please verify your email: {notification.Email } by clicking the link: {verificationLink}";
+        var message = $"Thank you for registering, please verify your email: {notification.Email} by clicking the link: {verificationLink}";
         _emailSender.SendEmailAsync(notification.Email, subject, message);
 
         return Task.CompletedTask;
